7895cfd18abcea0a22db9c8ed5428772a5e07ae1
julialfk <j.liemfk@gmail.com>
2024-02-12 16:24:38 +0100

    fixed server url

--- setup.py
+++ setup.py
@@ -12,10 +12,7 @@ VERSION = "1.0.0"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = [
-    "connexion",
-    "swagger-ui-bundle>=0.0.2"
-]
+REQUIRES = ["connexion", "swagger-ui-bundle>=0.0.2"]
 
 setup(
     name=NAME,
@@ -26,11 +23,10 @@ setup(
     keywords=["Swagger", "Simple Inventory API"],
     install_requires=REQUIRES,
     packages=find_packages(),
-    package_data={'': ['swagger/swagger.yaml']},
+    package_data={"": ["swagger/swagger.yaml"]},
     include_package_data=True,
-    entry_points={
-        'console_scripts': ['swagger_server=swagger_server.__main__:main']},
+    entry_points={"console_scripts": ["swagger_server=swagger_server.__main__:main"]},
     long_description="""\
     This is a simple API
-    """
+    """,
 )
